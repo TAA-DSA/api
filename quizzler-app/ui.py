1b1a1a91f3d4e4988d013012ef98722fdf84a694
Tasdeed <ayonaziz@gmail.com>
2025-04-23 13:53:37 -0400

    fixed the screen color changes

--- quizzler-app/ui.py
+++ quizzler-app/ui.py
@@ -4,53 +4,70 @@ from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
+"""Fix the bugs and complete the remaining programs"""
+
 
 class QuizUI:
     def __init__(self, quiz_brain: QuizBrain):
+
         self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
-        self.label = Label(text="score: 0", bg=THEME_COLOR, fg="white")
-        self.label.grid(row=0, column=1, sticky="e")
+        if self.quiz.still_has_questions:
 
-        self.canvas = Canvas(width=300, height=250, bg="white")
-        self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
+            self.label = Label(text="score: 0", bg=THEME_COLOR, fg="white")
+            self.label.grid(row=0, column=1, sticky="e")
 
-        true_image = PhotoImage(file="./images/true.png")
-        self.btn_true = Button(image=true_image, command=self.answer_true)
+            self.canvas = Canvas(width=300, height=250, bg="white")
+            self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
 
-        self.btn_true.grid(row=3, column=0)
+            self.score_text = self.canvas.create_text(
+                150,
+                125,
+                width=280,
+                text="Hello",
+                fill="black",
+                font=("Ariel", 20, "italic"),
+            )
 
-        false_image = PhotoImage(file="./images/false.png")
-        self.btn_false = Button(image=false_image, command=self.answer_false)
+            true_image = PhotoImage(file="./images/true.png")
+            self.btn_true = Button(image=true_image, command=self.answer_true)
 
-        self.btn_false.grid(row=3, column=1)
+            self.btn_true.grid(row=3, column=0)
 
-        self.score_text = self.canvas.create_text(
-            150,
-            125,
-            width=280,
-            text="Hello",
-            fill="black",
-            font=("Ariel", 20, "italic"),
-        )
+            false_image = PhotoImage(file="./images/false.png")
+            self.btn_false = Button(image=false_image, command=self.answer_false)
+            self.btn_false.grid(row=3, column=1)
 
-        self.get_next_question()
+            self.get_next_question()
+        else:
+            self.canvas.itemconfig(
+                self.score_text, text="quiz completed, thank you for playing"
+            )
 
         self.window.mainloop()
 
     def get_next_question(self):
+        self.canvas.config(bg="white")
         q_text = self.quiz.next_question()
-        print(q_text)
         self.canvas.itemconfig(self.score_text, text=q_text)
 
     def answer_true(self):
-        self.quiz.check_answer(user_answer="true")
+        self.feedback(self.quiz.check_answer(user_answer="true"))
         self.label.config(text=f"score: {self.quiz.score}")
-        self.get_next_question()
 
     def answer_false(self):
-        self.quiz.check_answer(user_answer="false")
-        self.get_next_question()
+        is_right = "false"
+        self.quiz.check_answer(user_answer=is_right)
+        self.feedback()
+
+    def feedback(self, is_true):
+        self.is_true = is_true
+        if is_true:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+
+        self.window.after(1000, self.get_next_question)
