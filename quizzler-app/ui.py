a380c34937002532989d68eb3822fa583ca6c842
Tasdeed <ayonaziz@gmail.com>
2025-04-23 02:11:47 -0400

    implemented btn function

--- quizzler-app/ui.py
+++ quizzler-app/ui.py
@@ -1,11 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 
 THEME_COLOR = "#375362"
 
 
 class QuizUI:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -16,16 +18,39 @@ class QuizUI:
         self.canvas = Canvas(width=300, height=250, bg="white")
         self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
 
-        self.true_image = PhotoImage(file="./images/true.png")
-        self.btn = Button(image=self.true_image, command="")
-        self.btn.grid(row=3, column=0)
+        true_image = PhotoImage(file="./images/true.png")
+        self.btn_true = Button(image=true_image, command=self.answer_true)
 
-        self.false_image = PhotoImage(file="./images/false.png")
-        self.btn = Button(image=self.false_image, command="")
-        self.btn.grid(row=3, column=1)
+        self.btn_true.grid(row=3, column=0)
+
+        false_image = PhotoImage(file="./images/false.png")
+        self.btn_false = Button(image=false_image, command=self.answer_false)
+
+        self.btn_false.grid(row=3, column=1)
 
         self.score_text = self.canvas.create_text(
-            150, 125, text="Hello", fill="black", font=("Ariel", 20, "italic")
+            150,
+            125,
+            width=280,
+            text="Hello",
+            fill="black",
+            font=("Ariel", 20, "italic"),
         )
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        print(q_text)
+        self.canvas.itemconfig(self.score_text, text=q_text)
+
+    def answer_true(self):
+        self.quiz.check_answer(user_answer="true")
+        self.label.config(text=f"score: {self.quiz.score}")
+        self.get_next_question()
+
+    def answer_false(self):
+        self.quiz.check_answer(user_answer="false")
+        self.get_next_question()
