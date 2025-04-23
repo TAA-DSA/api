7f46e4eaa808d6bb7e151e9f9f97c0fc3b2b938c
Tasdeed <ayonaziz@gmail.com>
2025-04-23 15:16:15 -0400

    completed the functionality for the quiz app

--- quizzler-app/ui.py
+++ quizzler-app/ui.py
@@ -15,44 +15,44 @@ class QuizUI:
         self.window.title("Quizler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
-        if self.quiz.still_has_questions:
+        self.label = Label(text="score: 0", bg=THEME_COLOR, fg="white")
+        self.label.grid(row=0, column=1, sticky="e")
 
-            self.label = Label(text="score: 0", bg=THEME_COLOR, fg="white")
-            self.label.grid(row=0, column=1, sticky="e")
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
 
-            self.canvas = Canvas(width=300, height=250, bg="white")
-            self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
+        self.score_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="Hello",
+            fill="black",
+            font=("Ariel", 20, "italic"),
+        )
 
-            self.score_text = self.canvas.create_text(
-                150,
-                125,
-                width=280,
-                text="Hello",
-                fill="black",
-                font=("Ariel", 20, "italic"),
-            )
+        true_image = PhotoImage(file="./images/true.png")
+        self.btn_true = Button(image=true_image, command=self.answer_true)
 
-            true_image = PhotoImage(file="./images/true.png")
-            self.btn_true = Button(image=true_image, command=self.answer_true)
+        self.btn_true.grid(row=3, column=0)
 
-            self.btn_true.grid(row=3, column=0)
-
-            false_image = PhotoImage(file="./images/false.png")
-            self.btn_false = Button(image=false_image, command=self.answer_false)
-            self.btn_false.grid(row=3, column=1)
-
-            self.get_next_question()
-        else:
-            self.canvas.itemconfig(
-                self.score_text, text="quiz completed, thank you for playing"
-            )
-
+        false_image = PhotoImage(file="./images/false.png")
+        self.btn_false = Button(image=false_image, command=self.answer_false)
+        self.btn_false.grid(row=3, column=1)
+        self.get_next_question()
         self.window.mainloop()
 
     def get_next_question(self):
-        self.canvas.config(bg="white")
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.score_text, text=q_text)
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.config(bg="white")
+            self.canvas.itemconfig(self.score_text, text=q_text)
+        else:
+            self.canvas.config(bg="white")
+            self.canvas.itemconfig(
+                self.score_text, text="Quiz end, Thank you for playing."
+            )
+            self.btn_true.config(state="disabled")
+            self.btn_false.config(state="disabled")
 
     def answer_true(self):
         self.feedback(self.quiz.check_answer(user_answer="true"))
